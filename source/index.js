7c8316a1c3150eee3aa680d75e6c165fa09b150e
Edgar <edfabrizio05@gmail.com>
2021-09-04 01:53:07 -0500

    carpeta source donde vamos a encontrar varios archivos del funcionamiento del servidor

--- source/index.js
+++ source/index.js
@@ -1,6 +1,7 @@
-import express from "express";
+import express from "express"
 
 const app= express()
 
 app.listen(3000)
-consolo.log('Server listen on port' , 3000)
+
+console.log('Server listen on port' , 3000)
