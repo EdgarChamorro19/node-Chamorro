5bd9aed5c01bf812255388a0b2d55993fbaa527b
Edgar <edfabrizio05@gmail.com>
2021-09-05 01:28:53 -0500

    Actualizado mandando datos del producto y viendolo en el servidor

--- source/controllers/products.controller.js
+++ source/controllers/products.controller.js
@@ -1,10 +1,12 @@
+import Product from "../models/products";
 
 export const createProduct= (req,res) =>{
-
+    console.log(req.body)
+    res.json('Creando Producto')
 }
 
 export const getProducts= (req,res) =>{
-
+    res.json('Obtener Producto')
 }
 
 export const getProductsById= (req,res) =>{
--- source/database.js
+++ source/database.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose" 
 
-mongoose.connect("mongodb://Localhost/companydb")
+mongoose.connect("mongodb://Localhost/companydb",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
     .then(db => console.log('Base de datos Conectada'))
     .catch(error => console.log(error))
