95f63ca76b87e4db19c1be5ae4b3c623c562a45b
Edgar <edfabrizio05@gmail.com>
2021-09-05 23:11:16 -0500

    Haciendo y Probando el signin

--- source/controllers/auth.controller.js
+++ source/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/User'
 import jwt from "jsonwebtoken";
 import config from "../config";
+import Role from '../models/role';
 
 export const signUp= async (req,res)=>{
     const {username, email, password, roles} = req.body
@@ -12,6 +13,14 @@ export const signUp= async (req,res)=>{
         password: await User.encryptPassword(password)
     })
     
+    if(roles){
+        const foundRoles=await Role.find({name: {$in: roles}})
+        newUser.roles= foundRoles.map(role => role._id)
+    }else{
+        const role= await Role.findOne({name: "user"})
+        newUser.roles = [role._id]
+    }
+
     const saveUser= await newUser.save()
 
     const token= jwt.sign({id:saveUser._id},config.SECRET,{
@@ -21,5 +30,8 @@ export const signUp= async (req,res)=>{
 }
 
 export const signIn= async (req,res)=>{
-    res.json('singin')
+    const userFound= await User.findOne({email: req.body.email})
+    if (!userFound) return res.json.status(400).json({message: "Usuario no Encontrado"})
+    console.log(userFound)
+    res.json({token: ''})
 }
